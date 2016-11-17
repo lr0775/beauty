9473227fd71d51f8e80c8cb6652de3f951c588a8
lingrui <lingrui@stbl.cc>
2016-11-17 16:22:07 +0800

    提交代码

--- app/src/main/java/com/xxxiao/beauty/activity/SearchActivity.java
+++ app/src/main/java/com/xxxiao/beauty/activity/SearchActivity.java
@@ -3,8 +3,7 @@ package com.xxxiao.beauty.activity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.SearchView;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.support.v7.widget.Toolbar;
 
 import com.xxxiao.beauty.R;
 import com.xxxiao.beauty.base.BaseActivity;
@@ -17,37 +16,30 @@ import java.net.URLEncoder;
 public class SearchActivity extends BaseActivity {
 
     private FragmentManager mFragmentManager;
-    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
         mFragmentManager = getSupportFragmentManager();
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
-        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        searchView.setIconifiedByDefault(false);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final SearchView searchView = (SearchView) toolbar.findViewById(R.id.search_view);
+        searchView.onActionViewExpanded();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                searchView.clearFocus();
+                searchView.onActionViewCollapsed();
                 try {
                     String keyword = URLEncoder.encode(query, "UTF-8");
                     mFragmentManager
                             .beginTransaction()
                             .replace(R.id.container, MainFragment.newInstance(API.SEARCH + keyword))
                             .commit();
-                    return true;
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
-                    return false;
                 }
+                return false;
             }
 
             @Override
@@ -55,6 +47,6 @@ public class SearchActivity extends BaseActivity {
                 return false;
             }
         });
-        return true;
     }
+
 }
